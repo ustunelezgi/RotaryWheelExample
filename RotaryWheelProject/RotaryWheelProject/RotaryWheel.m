046ba186791bd41e6dced21ada4519a47a3c26a9
ustunelezgi <ezgi.ustunel@kokteyl.com>
2020-12-14 18:18:09 +0300

    spinning

--- RotaryWheelProject/RotaryWheelProject/RotaryWheel.m
+++ RotaryWheelProject/RotaryWheelProject/RotaryWheel.m
@@ -17,6 +17,7 @@ static float deltaAngle;
 
 @implementation RotaryWheel {
     NSInteger counter;
+    UIImageView *im;
 }
 
 @synthesize startTransform;
@@ -38,183 +39,98 @@ static float deltaAngle;
     return self;
 }
 
+// an ivar for your class:
+BOOL animating;
+
+- (void)spinWithOptions:(UIViewAnimationOptions)options duration:(float)duration {
+    // this spin completes 360 degrees every 2 seconds
+    [UIView animateWithDuration:duration
+                          delay:0
+                        options:options
+                     animations:^{
+        self->im.transform = CGAffineTransformRotate(self->im.transform, M_PI / 2);
+    }
+                     completion:^(BOOL finished) {
+        if (finished) {
+            if (animating) {
+                // if flag still set, keep spinning with constant speed
+                [self spinWithOptions:UIViewAnimationOptionCurveLinear duration:duration];
+            } else if (options != UIViewAnimationOptionCurveEaseOut) {
+                // one last spin, with deceleration
+                [self spinWithOptions: UIViewAnimationOptionCurveEaseOut duration:2.0];
+            }
+        }
+    }];
+    
+}
+
+- (void)startSpin {
+    if (!animating) {
+        animating = YES;
+        [self spinWithOptions: UIViewAnimationOptionCurveEaseIn duration:0.3];
+    }
+}
+
+- (void)continueSpin {
+    if (!animating) {
+        animating = YES;
+        [self spinWithOptions: UIViewAnimationOptionCurveEaseIn duration:0.5];
+    }
+}
+
+- (void)stopSpin {
+    // set the flag to stop spinning after one last 90 degree increment
+    animating = NO;
+    
+}
+
 - (void)drawWheel {
     container = [[UIView alloc] initWithFrame:self.frame];
     CGFloat angleSize = 2*M_PI/numberOfSections;
     
     container.userInteractionEnabled = NO;
+    
     [self addSubview:container];
     
-    for (int i = 0; i < numberOfSections; i++) {
-        UIImageView *im = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"segment.png"]];
-        
-        im.layer.anchorPoint = CGPointMake(1.0f, 0.5f);
-        im.layer.position = CGPointMake(container.bounds.size.width/2.0,
-                                        container.bounds.size.height/2.0);
-        im.transform = CGAffineTransformMakeRotation(angleSize * i);
-        im.alpha = 0.6;
-        im.tag = i;
-        [container addSubview:im];
-    }
+    im = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"wheel"]];
+    im.frame = CGRectMake(container.center.x, container.center.y, 277, 283);
+    im.layer.position = CGPointMake(container.bounds.size.width/2.0,
+                                    container.bounds.size.height/2.0);
     
-    UIButton *button = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, 58, 58)];
-    [button setImage:[UIImage imageNamed:@"tıkla_kazan"] forState:normal];
-    button.center = self.center;
-    [button addTarget:self action:@selector(tıklaKazan:) forControlEvents:UIControlEventTouchUpInside];
-    button.center = CGPointMake(button.center.x, button.center.y+3);
-    [self addSubview:button];
+    [container addSubview:im];
     
-    sectors = [NSMutableArray arrayWithCapacity:numberOfSections];
-    if (numberOfSections % 2 == 0) {
-        [self buildSectorsEven];
-    } else {
-        [self buildSectorsOdd];
-    }
-    
-    [self.delegate wheelDidChangeValue:[NSString stringWithFormat:@"value is %i", self.currentSector]];
-}
-
-- (void)tıklaKazan:(UIButton *)sender {
-    [self setTimer];
-}
-
-- (void)setTimer {
-    
-    for (float i=0.2; i<5.0; i+=0.1) {
-        //NSLog(@"i değeri : %f",i);
-        [NSTimer scheduledTimerWithTimeInterval:i
-                                         target:self
-                                       selector:@selector(rotate)
-                                       userInfo:nil
-                                        repeats:NO];
-    }
-}
-
-- (void)rotate {
-    CGAffineTransform t = CGAffineTransformRotate(container.transform, -0.30);
-    container.transform = t;
 }
 
 - (BOOL)beginTrackingWithTouch:(UITouch *)touch withEvent:(UIEvent *)event {
-    
     CGPoint touchPoint = [touch locationInView:self];
-    float dx = touchPoint.x - container.center.x;
-    float dy = touchPoint.y - container.center.y;
+    float dx = touchPoint.x - im.center.x;
+    float dy = touchPoint.y - im.center.y;
     deltaAngle = atan2(dy, dx);
-    startTransform = container.transform;
+    startTransform = im.transform;
     
-    float dist = [self calculateDistanceFromCenter:touchPoint];
-    // 1.2 - Filter out touches too close to the center
-    if (dist < 40 || dist > 200) {
-        // forcing a tap to be on the ferrule
-        //NSLog(@"ignoring tap (%f,%f)", touchPoint.x, touchPoint.y);
-        return NO;
-    }
+    [self startSpin];
     
     return YES;
 }
 
 - (BOOL)continueTrackingWithTouch:(UITouch *)touch withEvent:(UIEvent *)event {
+    
     CGFloat radians = atan2f(container.transform.b, container.transform.a);
     //NSLog(@"rad is %f", radians);
-    
     CGPoint pt = [touch locationInView:self];
-    float dx = pt.x  - container.center.x;
-    float dy = pt.y  - container.center.y;
+    float dx = pt.x  - im.center.x;
+    float dy = pt.y  - im.center.y;
     float ang = atan2(dy,dx);
     NSLog(@"angle: %f", ang);
     float angleDifference = deltaAngle - ang;
-    container.transform = CGAffineTransformRotate(startTransform, -angleDifference);
+    
+    double delayInSeconds = 5.0;
+    dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC));
+    dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
+        [self stopSpin];
+    });
+    
     return YES;
-}
-
-- (void)endTrackingWithTouch:(UITouch *)touch withEvent:(UIEvent *)event {
-    // 1 - Get current container rotation in radians
-    CGFloat radians = atan2f(container.transform.b, container.transform.a);
-    // 2 - Initialize new value
-    CGFloat newVal = 0.0;
-    // 3 - Iterate through all the sectors
-    for (Sector *s in sectors) {
-        // 4 - Check for anomaly (occurs with even number of sectors)
-        if (s.minValue > 0 && s.maxValue < 0) {
-            if (s.maxValue > radians || s.minValue < radians) {
-                // 5 - Find the quadrant (positive or negative)
-                if (radians > 0) {
-                    newVal = radians - M_PI;
-                } else {
-                    newVal = M_PI + radians;
-                }
-                currentSector = s.sector;
-            }
-        }
-        // 6 - All non-anomalous cases
-        else if (radians > s.minValue && radians < s.maxValue) {
-            newVal = radians - s.midValue;
-            currentSector = s.sector;
-        }
-    }
-    // 7 - Set up animation for final rotation
-    [UIView beginAnimations:nil context:NULL];
-    [UIView setAnimationDuration:0.2];
-    CGAffineTransform t = CGAffineTransformRotate(container.transform, -newVal);
-    container.transform = t;
-    [UIView commitAnimations];
-}
-
-- (float) calculateDistanceFromCenter:(CGPoint)point {
-    CGPoint center = CGPointMake(self.bounds.size.width/2, self.bounds.size.height/2);
-    float dx = point.x - center.x;
-    float dy = point.y - center.y;
-    return sqrt(dx*dx + dy*dy);
-}
-
-- (void) buildSectorsOdd {
-    // 1 - Define sector length
-    CGFloat fanWidth = M_PI*2/numberOfSections;
-    // 2 - Set initial midpoint
-    CGFloat mid = 0;
-    // 3 - Iterate through all sectors
-    for (int i = 0; i < numberOfSections; i++) {
-        Sector *sector = [[Sector alloc] init];
-        // 4 - Set sector values
-        sector.midValue = mid;
-        sector.minValue = mid - (fanWidth/2);
-        sector.maxValue = mid + (fanWidth/2);
-        sector.sector = i;
-        mid -= fanWidth;
-        if (sector.minValue < - M_PI) {
-            mid = -mid;
-            mid -= fanWidth;
-        }
-        // 5 - Add sector to array
-        [sectors addObject:sector];
-        //NSLog(@"cl is %@", sector);
-    }
-}
-
-- (void) buildSectorsEven {
-    // 1 - Define sector length
-    CGFloat fanWidth = M_PI*2/numberOfSections;
-    // 2 - Set initial midpoint
-    CGFloat mid = 0;
-    // 3 - Iterate through all sectors
-    for (int i = 0; i < numberOfSections; i++) {
-        Sector *sector = [[Sector alloc] init];
-        // 4 - Set sector values
-        sector.midValue = mid;
-        sector.minValue = mid - (fanWidth/2);
-        sector.maxValue = mid + (fanWidth/2);
-        sector.sector = i;
-        if (sector.maxValue-fanWidth < - M_PI) {
-            mid = M_PI;
-            sector.midValue = mid;
-            sector.minValue = fabsf(sector.maxValue);
-        }
-        mid -= fanWidth;
-        //NSLog(@"cl is %@", sector);
-        // 5 - Add sector to array
-        [sectors addObject:sector];
-    }
 }
 
 @end
--- RotaryWheelProject/RotaryWheelProject/ViewController.m
+++ RotaryWheelProject/RotaryWheelProject/ViewController.m
@@ -23,15 +23,15 @@
     //sectorLabel = [[UILabel alloc] initWithFrame:CGRectMake(50, 400, 120, 30)];
     //sectorLabel.textAlignment = UITextAlignmentCenter;
     [self.view addSubview:sectorLabel];
+    self.view.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"bg"]];
     // 3 - Set up rotary wheel
     RotaryWheel *wheel = [[RotaryWheel alloc] initWithFrame:CGRectMake(self.view.center.x/6, self.view.center.y/4, 200, 200)
                                                 andDelegate:self
-                                               withSections:10];
+                                               withSections:6];
     wheel.center = CGPointMake(160, 240);
     // 4 - Add wheel to view
     [self.view addSubview:wheel];
 }
-
 
 - (void)wheelDidChangeValue:(nonnull NSString *)newValue {
     self.sectorLabel.text = newValue;
